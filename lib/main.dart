51f40b0ef05bb233edce1d52dba3dc608c082d12
Alif Suryadharma Firdaus <112613505+AlifSuryadharmaF@users.noreply.github.com>
2023-10-28 17:12:23 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'ui/bergabung.dart';
 import 'ui/with_data_page.dart';
 import 'ui/home.dart';
 import 'ui/lainnya.dart';
+import 'ui/preview.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +22,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(title: 'Beranda'),
-        '/lainnya': (context) => Another(title: 'Lainnya'),
+        '/lainnya': (context) => const Another(title: 'Lainnya'),
+        '/bergabung': (context) => const JoinMeet(title: 'Bergabung'),
+        '/preview': (context) => const PreviewMeet(title: 'Preview'),
         '/withdata': (context) => const WithDataPage(title: 'Dengan Data')
       },
     );
